ea22ad19f667f8235b4c6578d02f943ba0198154
EnriqueSubias <48204227+EnriqueSubias@users.noreply.github.com>
2022-11-13 20:01:57 +0100

    Getting started with js

---
+++ script.js
@@ -0,0 +1,31 @@
+const minesweeper = { // Avoids polluting the global namespace
+    init: function () {
+        console.log('HTML page is fully loaded');
+
+    }
+}
+
+// window.addEventListener("load", minesweeper.init);
+window.onload = (event) => {
+    minesweeper.init();
+};
+// window.onerror
+
+// TODO: Add different functions for header, playfield, buttons and footer
+
+const body = document.querySelector('body');
+
+const header = document.createElement('header');
+
+const divPlayfiled = document.createElement('div');
+divPlayfiled.id = 'playfield';
+
+const divButtons = document.createElement('div');
+divButtons.id = 'buttons';
+
+const footer = document.createElement('footer');
+
+body.appendChild(header);
+body.appendChild(divPlayfiled);
+body.appendChild(divButtons);
+body.appendChild(footer);
