4905b8ac18b367db1433bcfa1d8fc8eec13c5d85
EnriqueSubias <48204227+EnriqueSubias@users.noreply.github.com>
2022-12-16 00:10:54 +0100

    Plain 9x9 playfield generated

--- script.js
+++ script.js
@@ -5,73 +5,155 @@ window.addEventListener("load", () => {
 
 const minesweeper = { // Avoids polluting the global namespace
 
-    // Creates the header
-    createHeader: function () {
+    gameTypes: [
+        { name: 'small', size: 9, mines: 10 },
+        { name: 'medium', size: 16, mines: 40 },
+        { name: 'large', size: 24, mines: 150 }
+    ],
+
+    // Builds the header
+    buildHeader: function () {
         const header = document.createElement('header');
-        
-        const divHeaderTitle = document.createElement('div');
-        divHeaderTitle.id = 'header-title';
 
-        const h1 = document.createElement('h1');
-        h1.textContent = 'Minesweeper';
+        const divHeader = document.createElement('div');
+        header.appendChild(divHeader);
+        divHeader.id = 'header-title';
 
-        const p = document.createElement('p');
-        p.textContent = 'of Enrique Alejo Subías Melgar';
+        const heading1 = document.createElement('h1');
+        divHeader.appendChild(heading1);
+        heading1.innerText = 'Minesweeper';
 
-        divHeaderTitle.appendChild(h1);
-        divHeaderTitle.appendChild(p);
-
-        header.appendChild(divHeaderTitle);
+        const paragraph = document.createElement('p');
+        divHeader.appendChild(paragraph);
+        paragraph.innerText = 'of Enrique Alejo Subías Melgar';
 
         return header;
     },
 
-    // Creates the playfield
-    createPlayfield: function () {
+    // Builds the playfield
+    buildPlayfield: function () {
         const divPlayfiled = document.createElement('div');
         divPlayfiled.id = 'playfield';
+
+        
+
         return divPlayfiled;
     },
 
-    // Creates the buttonbar
-    createButtons: function () {
+    // Builds the buttonbar
+    buildButtons: function () {
         const divButtons = document.createElement('div');
         divButtons.id = 'buttons';
+
+        const buttonSmall = this.buildButton('button', 'game-small', 'Small');
+        divButtons.appendChild(buttonSmall);
+
+        const buttonMedium = this.buildButton('button', 'game-medium', 'Medium');
+        divButtons.appendChild(buttonMedium);
+
+        const buttonLarge = this.buildButton('button', 'game-large', 'Large');
+        divButtons.appendChild(buttonLarge);
+
         return divButtons;
     },
 
-    // Creates the footer
-    createFooter: function () {
+    // Builds a single button
+    buildButton: function (className, id, label) {
+        const button = document.createElement('button');
+        button.className = className;
+        button.innerText = label;
+
+        return button;
+    },
+
+    // Builds the footer
+    buildFooter: function () {
         const footer = document.createElement('footer');
+
+        const divFooter = document.createElement('div');
+        footer.appendChild(divFooter);
+        divFooter.id = 'footer-title';
+
+        const paragraph = document.createElement('p');
+        divFooter.appendChild(paragraph);
+        paragraph.innerHTML = '&copy; 2022 by Enrique Alejo Subías Melgar';
+
         return footer;
     },
 
-    init: function () {
-        console.log('HTML page is fully loaded');
-        // alert('HTML page is fully loaded');
 
+    // Starts a new game and then initializes the playfield with the given size
+    generatePlayfield: function (size) {
+
+        // small: 9x9, 10 mines
+        // medium: 16x16, 40 mines
+        // large: 24x24, 150 mines
+
+        // The playfield will be reused:
+        // - find it
+        // - empty it
+        // - fill it
+
+        const playfield = document.querySelector('#playfield'); // Finds the playfield div
+
+        playfield.innerHTML = ''; // Removes all children from the playfield
+
+        // Fillthe playfield with cells with 2 nested loops
+        
+        size = 9;
+
+        // Creates a new playfield
+        for (let i = 0; i < size; i++) {
+            for (let j = 0; j < size; j++) {
+                playfield.appendChild(this.generateCell(i, j));
+            }
+        }
+
+    },
+
+    // Generates a single cell
+    generateCell: function (row, colunm) {
+        const cell = document.createElement('div');
+        cell.className = 'cell';
+        cell.className += ' covered';
+        
+        cell.dataset.x = colunm;
+        cell.dataset.y = row;
+
+        return cell;
+    },
+
+
+
+    init: function () {
         const body = document.querySelector('body'); // Finds the body element
 
-        // Creates a div element with class="content"
+        // Creates the main content div
         const content = document.createElement('div');
-        content.className = 'content';
         body.appendChild(content);
+        content.className = 'content';
 
         // header
-        const header = this.createHeader();
+        const header = this.buildHeader();
+        content.appendChild(header);
 
         // div#playfield
-        const divPlayfiled = this.createPlayfield();
+        const divPlayfiled = this.buildPlayfield();
+        content.appendChild(divPlayfiled);
 
         // div#buttonbar
-        const divButtons = this.createButtons();
+        const divButtons = this.buildButtons();
+        content.appendChild(divButtons);
 
         // footer
-        const footer = this.createFooter();
-
-        content.appendChild(header);
-        content.appendChild(divPlayfiled);
-        content.appendChild(divButtons);
+        const footer = this.buildFooter();
         content.appendChild(footer);
+
+
+
+        // Method that fills the playfield and brings an argument that is the size of the playfield
+        this.generatePlayfield('small');
+
+
     }
 }
