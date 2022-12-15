26dbc0ca26e5a25e1e379e3ba1cef845748cbf39
EnriqueSubias <48204227+EnriqueSubias@users.noreply.github.com>
2022-12-16 00:52:05 +0100

    Generates 3 different game sizes correctly

--- script.js
+++ script.js
@@ -35,8 +35,6 @@ const minesweeper = { // Avoids polluting the global namespace
         const divPlayfiled = document.createElement('div');
         divPlayfiled.id = 'playfield';
 
-        
-
         return divPlayfiled;
     },
 
@@ -48,11 +46,23 @@ const minesweeper = { // Avoids polluting the global namespace
         const buttonSmall = this.buildButton('button', 'game-small', 'Small');
         divButtons.appendChild(buttonSmall);
 
+        buttonSmall.addEventListener('click', () => {
+            this.newGame("small");
+        });
+
         const buttonMedium = this.buildButton('button', 'game-medium', 'Medium');
         divButtons.appendChild(buttonMedium);
 
+        buttonMedium.addEventListener('click', () => {
+            this.newGame('medium');
+        });
+
         const buttonLarge = this.buildButton('button', 'game-large', 'Large');
         divButtons.appendChild(buttonLarge);
+
+        buttonLarge.addEventListener('click', () => {
+            this.newGame('large');
+        });
 
         return divButtons;
     },
@@ -81,30 +91,41 @@ const minesweeper = { // Avoids polluting the global namespace
         return footer;
     },
 
+    gameType: function (size) {
+        // for each
+        for (let game = 0; game < this.gameTypes.length; game++) {
+            if (this.gameTypes[game].name === size) {
+
+                this.size = this.gameTypes[game].size;
+                this.mines = this.gameTypes[game].mines;
+
+                console.log(this.gameTypes[game].name + ' game type selected')
+                console.log(this.gameTypes[game])
+            }
+        }
+        // return this.gameTypes.find((gameType) => gameType.name === size);
+    },
+
+    // Starts a new game
+    newGame: function (size) {
+        this.generatePlayfield(size);
+
+    },
 
     // Starts a new game and then initializes the playfield with the given size
     generatePlayfield: function (size) {
 
-        // small: 9x9, 10 mines
-        // medium: 16x16, 40 mines
-        // large: 24x24, 150 mines
+        this.gameType(size);
 
-        // The playfield will be reused:
-        // - find it
-        // - empty it
-        // - fill it
+        console.log('Generating playfield of size ' + this.size + 'x' + this.size + ' with ' + this.mines + ' mines')
 
         const playfield = document.querySelector('#playfield'); // Finds the playfield div
 
-        playfield.innerHTML = ''; // Removes all children from the playfield
+        playfield.innerHTML = ''; // Empties the playfield
 
-        // Fillthe playfield with cells with 2 nested loops
-        
-        size = 9;
-
-        // Creates a new playfield
-        for (let i = 0; i < size; i++) {
-            for (let j = 0; j < size; j++) {
+        // Fills the new playfield with cells
+        for (let i = 0; i < this.size; i++) {
+            for (let j = 0; j < this.size; j++) {
                 playfield.appendChild(this.generateCell(i, j));
             }
         }
@@ -116,9 +137,14 @@ const minesweeper = { // Avoids polluting the global namespace
         const cell = document.createElement('div');
         cell.className = 'cell';
         cell.className += ' covered';
-        
+
         cell.dataset.x = colunm;
         cell.dataset.y = row;
+
+        const style = `calc((100% / ${this.size}) - (2 * var(--shadowsize)))`;
+
+        cell.style.width = style;
+        cell.style.height = style;
 
         return cell;
     },
@@ -148,12 +174,6 @@ const minesweeper = { // Avoids polluting the global namespace
         // footer
         const footer = this.buildFooter();
         content.appendChild(footer);
-
-
-
-        // Method that fills the playfield and brings an argument that is the size of the playfield
-        this.generatePlayfield('small');
-
 
     }
 }
